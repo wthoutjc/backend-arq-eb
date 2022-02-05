7edaa7da15ce134994cad55fc79c34162b91d5e7
wthoutjc <juancamilorr2012@hotmail.com>
2022-02-05 16:09:58 -0500

    .

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 # Desarrollado por: https://github.com/wthoutjc
 from flask import Flask, request, jsonify, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 # from mysql.connector.errors import Error
 
 # #JSON Web Tokens
@@ -22,15 +22,15 @@ from flask_mysqldb import MySQL
 
 application = Flask(__name__)
 
-application.config['MYSQL_HOST'] = 'database-arq.cc0sxaunqcsm.sa-east-1.rds.amazonaws.com'
-application.config['MYSQL_USER'] = 'admin'
-application.config['MYSQL_PASSWORD'] = 'Arq#2022'
-application.config['MYSQL_DB'] = 'db_arq'
-application.config['SECRET_KEY'] = 'UHGx14#&17NoPRQS#12'
+# application.config['MYSQL_HOST'] = 'database-arq.cc0sxaunqcsm.sa-east-1.rds.amazonaws.com'
+# application.config['MYSQL_USER'] = 'admin'
+# application.config['MYSQL_PASSWORD'] = 'Arq#2022'
+# application.config['MYSQL_DB'] = 'db_arq'
+# application.config['SECRET_KEY'] = 'UHGx14#&17NoPRQS#12'
 # application.config['JWT_SECRET_KEY'] = 'UHGx14#&17NoPRQS#12'
 
 
-mysql = MySQL(application)
+# mysql = MySQL(application)
 
 @application.route("/")
 def hello_world():
