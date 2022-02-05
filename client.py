228fd610ee576c75e66fd285fa5247652de21a11
wthoutjc <juancamilorr2012@hotmail.com>
2022-02-05 16:42:34 -0500

    client socket loaded

--- client.py
+++ client.py
@@ -12,63 +12,63 @@ import json
 # import eventlet
 
 # # Globals
-# socketio = None
+socketio = None
 # # thread = Thread()
 # # thread_stop_event = Event()
 
 # # Config LOGS
-# logging.basicConfig(filename='client.log', level=logging.DEBUG)
+logging.basicConfig(filename='client.log', level=logging.DEBUG)
 
-# class SocketIOClient(object):
-#     def __init__(self, app):
-#         global socketio
-#         self.app = app
-#         CORS(self.app)
-#         socketio = SocketIO(app, cors_allowed_origins='*') #cors_allowed_origins="*", async_mode="eventlet" http://frontend-arq.s3-website-sa-east-1.amazonaws.com/
-#         self.connected_clients = {}
+class SocketIOClient(object):
+    def __init__(self, app):
+        global socketio
+        self.app = app
+        CORS(self.app)
+        socketio = SocketIO(app, cors_allowed_origins='*') #cors_allowed_origins="*", async_mode="eventlet" http://frontend-arq.s3-website-sa-east-1.amazonaws.com/
+        self.connected_clients = {}
     
-#     def run(self):
-#         # Posible inclusión de Threads
+    def run(self):
+        # Posible inclusión de Threads
 
-#         # Rutas
-#         @socketio.on('connect')
-#         def on_connect():
-#             # global thread
-#             print(f'Cliente conectado satisfactoriamente')
+        # Rutas
+        @socketio.on('connect')
+        def on_connect():
+            # global thread
+            print(f'Cliente conectado satisfactoriamente')
 
-#             # if not thread.is_alive():
-#             #     thread = RandomThread()
-#             #     thread.start()
+            # if not thread.is_alive():
+            #     thread = RandomThread()
+            #     thread.start()
 
-#         @socketio.on('disconnect')
-#         def on_disconnect():
-#             print(f'Cliente desconectado satisfactoriamente.')
+        @socketio.on('disconnect')
+        def on_disconnect():
+            print(f'Cliente desconectado satisfactoriamente.')
 
-#         @socketio.on('messages')
-#         def on_messages(*args):
-#             response = [json.loads(data) for data in args]
-#             print(response)
-#             socketio.emit('message', {'message': 'Enviado desde backend'})
+        @socketio.on('messages')
+        def on_messages(*args):
+            response = [json.loads(data) for data in args]
+            print(response)
+            socketio.emit('message', {'message': 'Enviado desde backend'})
 
-#         # Conexión
-#         socketio.run(self.app, host="0.0.0.0", port=443, debug=True) #host="0.0.0.0"
-#         # eventlet.monkey_patch(socket=True, select=True)
+        # Conexión
+        socketio.run(self.app, host="0.0.0.0", port=443, debug=True) #host="0.0.0.0"
+        # eventlet.monkey_patch(socket=True, select=True)
 
-# class RandomThread(Thread):
-#     def __init__(self):
-#         self.delay = 1
-#         super(RandomThread, self).__init__()
+class RandomThread(Thread):
+    def __init__(self):
+        self.delay = 1
+        super(RandomThread, self).__init__()
 
-#     def random_number_generator(self):
-#         print(f'Generando aleatorios')
+    def random_number_generator(self):
+        print(f'Generando aleatorios')
 
-#         # while not thread_stop_event.is_set():
-#         #     number = round(random() * 10, 3)
-#         #     socketio.emit('new_number',  {'number': number}) # Pendiente por multi hilo (DE REQUERIRSE)
-#         #     thread_stop_event.set()
-#         #     sleep(self.delay)
+        # while not thread_stop_event.is_set():
+        #     number = round(random() * 10, 3)
+        #     socketio.emit('new_number',  {'number': number}) # Pendiente por multi hilo (DE REQUERIRSE)
+        #     thread_stop_event.set()
+        #     sleep(self.delay)
 
-#         print(f'End')
+        print(f'End')
 
-#     def start(self):
-#         self.random_number_generator()
+    def start(self):
+        self.random_number_generator()
