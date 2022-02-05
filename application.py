6324b12e3b8261742f9327c17701e57aad4a669b
wthoutjc <juancamilorr2012@hotmail.com>
2022-02-05 16:47:01 -0500

    routes updated

--- application.py
+++ application.py
@@ -38,9 +38,21 @@ class DecimalEncoder(json.JSONEncoder):
 # JSON WEB TOKEN
 jwt = JWTManager(app)
 
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    try:
+        print('Consultando token block list')
+        token, success = db_operations.consultar_token_blocklist(jti)
+        if success == True:
+            return token is not None
+    except Error as error:
+        print('Check token revoked Error: ' + str(error))
+        return False
+
 @app.route("/")
 def hello_world():
-    return 'Connected'
+    return make_response(jsonify({"res": 'Connected'}), 200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
