9bb335f037313a25ef54a769e3bb65e023c085ab
wthoutjc <juancamilorr2012@hotmail.com>
2022-02-05 13:01:06 -0500

    socket added

--- application.py
+++ application.py
@@ -1,5 +1,23 @@
 # Desarrollado por: https://github.com/wthoutjc
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+from mysql.connector.errors import Error
+
+#JSON Web Tokens
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager, get_jwt
+
+# Database
+from db.db import SQLOperations
+
+# Client
+# from client import SocketIOClient
+
+# Herramientas
+import json                     # Estructura json
+import datetime                 # Manejo de fechas
+from decouple import config
+
+from decimal import Decimal
 
 application = Flask(__name__)
 
